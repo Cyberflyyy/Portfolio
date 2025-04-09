a826a7ee3b2c7376f430b83a39eeefa789c34029
unknown <281425@student.pwr.edu.pl>
2025-04-09 14:31:30 +0200

    exp changesv2

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,10 @@ import About from "@/components/about";
 import Contact from "@/components/contact";
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
-import { LanguageSwitcher } from "@/components/language-switcher";
+
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
-import { useLanguage } from "@/context/language-context";
 
 export default function Home() {
   return (
